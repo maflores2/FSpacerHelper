5727786ddfff7bd71bd66e6b7f9db762d6d3ddcc
Max Flores <Max Flores>
2017-12-11 03:10:24 -0600

    Add project files.

---
+++ FSpacerHelper/Models/Operator.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FSpacerHelper.Models
+{
+    public class Operator
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ FSpacerHelper/Models/SearchInputModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSpacerHelper.Models
+{
+    public class SearchInputModel
+    {
+        public string SearchOperatorID { get; set; }
+        public string SearchDriveNumber { get; set; }
+
+    }
+}
